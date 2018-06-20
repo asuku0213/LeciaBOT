239f5d3da4d50b460f63c247437792a65be0972c
asuku0213 <39723622+asuku0213@users.noreply.github.com>
2018-06-19 23:06:19 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,8 +1,5 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-client.registry.registerGroup('random', 'Random');
-client.registry.registerCommandsIn(__dirname + "/commands");
-
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
