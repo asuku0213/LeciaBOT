646998d62c5bd09150f42ed426090a282097d556
asuku0213 <39723622+asuku0213@users.noreply.github.com>
2018-06-19 22:59:35 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,21 +1,8 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-client.on('ready', () => {
-    console.log('I am ready!');
-});
-
-client.on('message', message => {
-    if (message.content === 'こんにちは') {
-    	message.reply('こんにちはです！');
-  	}
-});
-
-client.on('message', message => {
-    if (message.content === 'おはよう') {
-    	message.reply('おはようございます！');
-  	}
-});
+client.registry.registerGroup('random', 'Random');
+client.registry.registerCommandsIn(__dirname + "/commands");
 
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
