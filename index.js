16a0b0e4978d3eb751658c574f143fa105740180
asuku0213 <39723622+asuku0213@users.noreply.github.com>
2018-06-20 17:33:43 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,39 @@
 const Discord = require('discord.js');
-const client = new Discord.Client();
+
+const PREFIX = "="
+
+var fortunes = [
+    "Yes",
+    "No",
+    "Maybe"
+],
+
+var client = new Discord.Client();
 
 client.on('ready', () => {
     console.log('I am ready!');
 });
 
-client.on('message', message => {
-    if (message.content === 'ping') {
-    	message.reply('pong');
+client.on('message', function(message) {
+    if (message.author.equals(bot.user)) return;
+    
+    if (!message.content.startsWith(PREFIX)) return;
+    
+    var args = message.content.substring(PREFIX.length).splits(" ");
+    
+    switch (args[0].toLowerCase()) {
+        case "ping":
+            message.channel.sendMessage("Pong!");
+            break;
+        case "info":
+            message.channel.sendMessage("I'm created by Asuku!);
+            break;
+        case "8ball":
+            if (args[1]) message.channel.sendMessage(fortunes[Math.floor(Math.random() * fortunes.length)]);
+            else message.channel.sendMessage("Can't read that")
+            break;
+        default:
+            message.channel.sendMessage("Invalid Command");
   	}
 });
 
