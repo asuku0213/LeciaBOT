49e4f1c561f6fd0819e911346636070eb3318161
asuku0213 <39723622+asuku0213@users.noreply.github.com>
2018-06-20 18:02:31 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const Discord = require('discord.js')
+const Discord = require('discord.js');
 const client = new Discord.Client();
 const PREFIX = "="
 
@@ -34,4 +34,5 @@ client.on('message', function(message) {
     }
 }
 
+// THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
