873ae56b509fe0cf8384d1f94eb5f9912e5ff22a
asuku0213 <39723622+asuku0213@users.noreply.github.com>
2018-06-20 18:03:43 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,38 +1,15 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const PREFIX = "="
-
-var fortunes = [
-    "Yes",
-    "No",
-    "Maybe"
-];
 
 client.on('ready', () => {
     console.log('I am ready!');
 });
 
-client.on('message', function(message) {
-    if (message.author.equals(bot.user)) return;
-
-    if (!message.content.startWith(PREFIX)) return;
-
-    var args = message.content.substring(PREFIX.length).split(" ");
-
-    switch (args[0].toLowerCase()){
-        case "ping":
-            message.channel.sendMessage("Pong!");
-            break;
-        case "info":
-            message.channel.sendMessage("Created by Asuku")
-            break;
-        case "8ball":
-            if (args[1] message.channel.sendMessage(fonrtunes[Math.floor(Math.random()*length)]);
-            break;
-        default:
-            message.channel.sendMessage("Invalid command");
-    }
-}
+client.on('message', message => {
+    if (message.content === 'ping') {
+    	message.reply('pong');
+  	}
+});
 
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
